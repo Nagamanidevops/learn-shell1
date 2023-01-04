be3d678be16521bf891cc8c5d39d189d04d3928d
nagamani <nagamanichavaaws@gmail.com>
2023-01-04 08:17:39 +0000

    colors added

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,19 @@
 echo helloworld
 
 echo " ***** helloworld ***** "
+
+#color code -e "/e[COLmMESSAGE\[0m"
+#green 31 -e "/e[COLmMESSAGE\[0m"
+#red  32 -e "/e[COLmMESSAGE\[0m"
+#green  33 -e "/e[COLmMESSAGE\[0m"
+#yellow  34 -e "/e[COLmMESSAGE\[0m"
+#blue  35 -e "/e[COLmMESSAGE\[0m"
+#cyan  31 -e "/e[COLmMESSAGE\[0m"
+
+echo  31 -e "/e[COLmMESSAGE\[0m"
+echo  32 -e "/e[COLmMESSAGE\[0m"
+echo  33 -e "/e[COLmMESSAGE\[0m"
+echo  34 -e "/e[COLmMESSAGE\[0m"
+
+
+
