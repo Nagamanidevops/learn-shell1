57814aeaff63772a7c40b73a45cad43d0aee552e
nagamani <nagamanichavaaws@gmail.com>
2023-01-06 09:59:41 +0000

    added new file

---
+++ 05-function.sh
@@ -0,0 +1,12 @@
+#functions are like variables
+#variable  when we assign a name to a set of data then it is a variable
+#function  when we assign a name to a set of commands then it is a variable
+
+#function declaration
+example()
+{
+    echo i am example of function
+}
+#call a fn
+
+example()
